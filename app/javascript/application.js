82d27e0ed8f96d4a000c485cb22574d8dea2aee6
4Pavlo2 <pshkoropad@gmail.com>
2022-02-18 18:34:38 +0200

    create new app with bootstrap\yarn\webpacker

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Entry point for the build script in your package.json
+import * as bootstrap from "bootstrap"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
