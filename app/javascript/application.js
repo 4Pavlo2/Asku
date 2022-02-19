72d5f2a97fc5ca2f459ca466c44913a7aedb9565
4Pavlo2 <pshkoropad@gmail.com>
2022-02-19 13:13:42 +0200

    add import Rails from "@rails/ujs" in aplication.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
+import Rails from "@rails/ujs"
 import * as bootstrap from "bootstrap"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
