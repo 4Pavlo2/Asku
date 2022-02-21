0185f6f9891d094d75589a31385e7227b6251b46
4Pavlo2 <pshkoropad@gmail.com>
2022-02-21 11:53:07 +0200

    improving controllers and & que

--- app/controllers/ answers_controller.rb
+++ app/controllers/ answers_controller.rb
@@ -8,7 +8,25 @@ class AnswersController < ApplicationController
       flash[:success] = "Answer created!"
       redirect_to question_path(@question)
     else
-      @answers = Answer.order created_at: :desc
+      @answers = @question.answers.order created_at: :desc
       render 'questions/show'
     end
   end
+
+  def destroy
+    answer = @question.answers.find params[:id]
+    answer.destroy
+    flash[:success] = "Answer deleted!"
+    redirect_to question_path(@question)
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def set_question!
+    @question = Question.find params[:question_id]
+  end
+end
