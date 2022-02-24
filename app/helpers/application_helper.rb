ee9d113c6ce449d765066ecbf0e3343a9f6ac942
4Pavlo2 <pshkoropad@gmail.com>
2022-02-24 14:42:11 +0200

    refactoring aplication.html \\ add helper \\ need fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,26 @@
 module ApplicationHelper
+  def nav_tab(title, url, options = {})
+  current_page = options.delete :current_page
+
+  css_class =current_page == title ? 'text-white' : 'text-secondary'
+
+  options[:class] = if options[:class]
+                        options[:class] + ' ' + css_class
+                      else
+                        css_class
+                      end
+    
+    link_to title, url, options
+  end
+
+  def currenly_at(current_page = '')
+    render partial: 'shared/menu', locals: {current_page: current_page}
+  end
+
   def full_title(page_title = "")
     base_title = "AskIT"
     if page_title.present?
-      "#{page_title} | | #{base_title}"
+      "#{page_title} | #{base_title}"
     else
       base_title
     end
