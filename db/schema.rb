4a660bc2cc1f568d7f214c25100f5047811d284b
4Pavlo2 <pshkoropad@gmail.com>
2022-03-02 15:25:25 +0200

    validation user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_02_111753) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_130327) do
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,8 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_111753) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
