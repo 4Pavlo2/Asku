039c3206bd3c2e1f463af27ae10b2b14a4bfdc8c
4Pavlo2 <pshkoropad@gmail.com>
2022-03-03 16:29:27 +0200

    les 9 alm ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resource :session, only: %i[new create]
+  resource :session, only: %i[new create destroy]
+
   resources :users, only: %i[new create edit update]
 
   resources :questions do
